4038dd0c07e4d9a778feb79d222b85be2254eb5d
HomelikeBrick42 <64717463+HomelikeBrick42@users.noreply.github.com>
2020-12-29 13:53:12 +1300

    Added Dummy Project

---
+++ PMCompiler/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace PMCompiler
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            while (true)
+            {
+                Console.Write("> ");
+                var line = Console.ReadLine();
+                if (string.IsNullOrWhiteSpace(line))
+                    return;
+
+                if (line == "1 + 2 * 3")
+                    Console.WriteLine("7");
+                else
+                    Console.WriteLine("ERROR: Invalid expression!");
+            }
+        }
+    }
+}
